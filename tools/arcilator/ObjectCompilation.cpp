c77727a2c2af158094dae6ab163b8c63785854a1
Hideto Ueno <uenoku.tokotoko@gmail.com>
2023-12-29 07:10:28 -0800

    save

--- tools/arcilator/ObjectCompilation.cpp
+++ tools/arcilator/ObjectCompilation.cpp
@@ -1,9 +1,12 @@
 #include "ObjectCompilation.h"
 #include "mlir/IR/Threading.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 #include "mlir/IR/BuiltinOps.h"
 void circt::compileLLVMModule(mlir::ModuleOp module){
     auto* context = module.getContext();
-    mlir::parallelForEach(&context);
-
+    for(auto module: module.getOps<mlir::func::FuncOp>())
+        {
+            // TODO: Compile it.
+        }
 }
